0b903b2fed987f53ca85c9289c29a6ce7b4cfa60
luismr-educarex <luismr@educarex.es>
2023-01-23 23:45:49 +0100

    Se cambia a sólo la ejecución de un solo consumidor

--- PSP/src/es/psp/unidad2/ejemplos/consumidor_productor_colas/ProductorConsumidorServicio.java
+++ PSP/src/es/psp/unidad2/ejemplos/consumidor_productor_colas/ProductorConsumidorServicio.java
@@ -8,15 +8,13 @@ public class ProductorConsumidorServicio {
     public static void main(String[] args) {
         //Crearmos una BlockingQueue de tamaño 10 mensajes
         BlockingQueue<Mensaje> queue = new ArrayBlockingQueue<>(10);
-        Productor producer = new Productor(queue);
-        Consumidor consumer = new Consumidor(queue,"consumidor1");
-        Consumidor consumer2 = new Consumidor(queue,"consumidor2");
+        Productor productor = new Productor(queue);
+        Consumidor consumidor = new Consumidor(queue,"consumidor1");
         //empieza el productor a producir mensajes en la cola
-        new Thread(producer).start();
+        new Thread(productor).start();
         //empieza el consumidor a consumir mensajes de la cola
-        
-        new Thread(consumer).start();
-        new Thread(consumer2).start();
+        new Thread(consumidor).start();
+       
         
         System.out.println("Productor y consumidores han empezado a ejecutarse");
     }
